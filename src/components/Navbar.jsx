be0144407e91fb66fb388b3c058836bfcb06c4e5
esanchezz74 <esanch747@gmail.com>
2020-09-24 13:34:02 -0400

    Added Navbar Skeleton

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { Component, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            SNCHZ <i class="fab fa-stripe-s"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                bruh🥵 products
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
