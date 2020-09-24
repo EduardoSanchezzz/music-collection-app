d7082c71aca96de15bb267636f13da48866c8bf0
esanchezz74 <esanch747@gmail.com>
2020-09-24 14:26:14 -0400

    Added 'dropdown' functionality and mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button.jsx";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <React.Fragment>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            SNCHZ <i class="fab fa-stripe-s"></i>
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <i className="fab fa-stripe-s"></i> NCHZ
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
@@ -40,7 +58,17 @@ const Navbar = () => {
                 bruh🥵 products
               </Link>
             </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </React.Fragment>
