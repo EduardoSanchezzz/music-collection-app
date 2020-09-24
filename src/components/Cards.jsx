f4f78f27650515ab1f4a8c494b108df21865485f
esanchezz74 <esanch747@gmail.com>
2020-09-24 18:29:27 -0400

    added cards and content

---
+++ src/components/Cards.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import CardItem from "./CardItem";
+import "./Cards.css";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Text</h1>
+      <div className="card__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-1.jpg"
+              text="Just a really long descrptioton of thos music like dammmnnnnnnn this bih long"
+              label="Music"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Just a really long descrptioton of thos music like dammmnnnnnnn this bih long"
+              label="Music"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-3.jpg"
+              text="Just a really long descrptioton of thos music like dammmnnnnnnn this bih long"
+              label="Music"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-4.jpg"
+              text="Just a really long descrptioton of thos music like dammmnnnnnnn this bih long"
+              label="Music"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-5.jpg"
+              text="Just a really long descrptioton of thos music like dammmnnnnnnn this bih long"
+              label="Music"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-6.jpg"
+              text="Just a really long descrptioton of thos music like dammmnnnnnnn this bih long"
+              label="Music"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
