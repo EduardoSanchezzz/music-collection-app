5f20a4e4baa035edff83c5ddd0a23f5c9f63b9f5
esanchezz74 <esanch747@gmail.com>
2020-09-24 20:19:37 -0400

    Updates

---
+++ precache-manifest.82f9ed7ddc9c02e11148d6d37b8a4a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0925603e6e5c8b464f89bdca1fce871",
+    "url": "/react-website-test/index.html"
+  },
+  {
+    "revision": "444fd36568db027bcd4e",
+    "url": "/react-website-test/static/css/main.60a57001.chunk.css"
+  },
+  {
+    "revision": "9bc0a0c5b68cca696e8c",
+    "url": "/react-website-test/static/js/2.184c25ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website-test/static/js/2.184c25ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444fd36568db027bcd4e",
+    "url": "/react-website-test/static/js/main.2bce9b4b.chunk.js"
+  },
+  {
+    "revision": "9166c8ba0cc2e17e6223",
+    "url": "/react-website-test/static/js/runtime-main.f846dd8d.js"
+  }
+]);
