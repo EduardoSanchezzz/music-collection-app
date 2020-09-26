7b40662b6d29387fdc4dcddc0b48812981f7b616
esanchezz74 <esanch747@gmail.com>
2020-09-26 12:53:55 -0400

    finished footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,26 +9,16 @@ const Footer = () => {
       <div className="footer-links">
         <div className="footer-links-wrapper">
           <div className="footer-link-items">
-            <h2>About Us</h2>
+            <h2>About</h2>
+            <Link to="/">About Me</Link>
             <Link to="/">How it Works</Link>
-            <Link to="/">Tests</Link>
-          </div>
-          <div className="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">How it Works</Link>
-            <Link to="/">Tests</Link>
           </div>
         </div>
         <div className="footer-links-wrapper">
           <div className="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">How it Works</Link>
-            <Link to="/">Tests</Link>
-          </div>
-          <div className="footer-link-items">
             <h2>Music</h2>
-            <Link to="/">How it Works</Link>
-            <Link to="/">Tests</Link>
+            <Link to="/">My Favourites</Link>
+            <Link to="/">Recently Featured</Link>
           </div>
         </div>
       </div>
@@ -36,7 +26,7 @@ const Footer = () => {
         <div className="social-media-wrap">
           <div className="footer-logo">
             <Link to="/" className="social-logo">
-              NCHZ
+              <i className="fab fa-stripe-s"></i>NCHZ
             </Link>
           </div>
           <small className="website-rights">Eduardo Sanchez © 2020</small>
@@ -49,6 +39,22 @@ const Footer = () => {
             >
               <i className="fab fa-instagram"></i>
             </a>
+            <a
+              className="social-icon-link facebook"
+              href="https://www.facebook.com/eddiesanchezzz/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook"></i>
+            </a>
+            <Link
+              className="social-icon-link snapchat"
+              to="/snapchat"
+              target="_blank"
+              aria-label="Snapchat"
+            >
+              <i class="fab fa-snapchat-square"></i>
+            </Link>
           </div>
         </div>
       </section>
