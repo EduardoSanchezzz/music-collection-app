d2152c4a34825e4b7957eaf4265649f90b223f17
esanchezz74 <esanch747@gmail.com>
2020-09-26 21:58:12 -0400

    fixed snapchat bug

--- src/components/pages/Snapchat.jsx
+++ src/components/pages/Snapchat.jsx
@@ -3,7 +3,7 @@ import "../../App.css";
 
 export function Snapchat() {
   return (
-    <div className="snapchat">
+    <div className="snapchat-page">
       <h1>ADD ME:</h1>
       <p className="username">@notsureeddie</p>
       <p>Or simply scan below</p>
